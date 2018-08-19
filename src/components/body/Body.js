8517d0939305a431de5255f9acfc5a4fd60efcd1
Garvit Khatri <garvitdelhi@gmail.com>
2018-08-19 08:45:11 +0530

    App restructuring and adding score

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Table from 'components/table/';
-import Timer from 'components/timer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -15,8 +11,7 @@ export default class Body extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      started: false,
-      evalute: false
+      started: false
     };
   }
 
@@ -50,23 +45,7 @@ export default class Body extends Component {
             Start
           </Button>}
         {this.state.started && 
-          <Card style={{margin: "50px", flexGrow: "1"}}>
-            <CardContent>
-              <Table evalute={this.state.evalute} />
-            </CardContent>
-            <CardActions>
-              <Timer stop={this.state.evalute} style={{paddingLeft: "24px"}}/>
-              <Button 
-                onClick={() => window.location.reload()}
-                style={{marginLeft: "auto"}}
-                color="primary"
-                variant="outlined"
-                size="small">
-                Reload
-              </Button>
-              <Button onClick={() => this.setState({evalute: true})} color="primary" variant="contained" size="small">Submit</Button>
-            </CardActions>
-          </Card>
+          <Table />
         }
       </div>
     );
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -7,6 +7,11 @@ import TableRow from '@material-ui/core/TableRow';
 import Input from '@material-ui/core/Input';
 import './table.css';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Timer from 'components/timer';
+import Button from '@material-ui/core/Button';
 
 export default class Body extends Component {
 
@@ -14,8 +19,10 @@ export default class Body extends Component {
     super(props);
     this.cols = Array.apply(null, Array(10)).map(() => Math.ceil(Math.random()*90) + 10);
     this.rows = Array.apply(null, Array(10)).map(() => Math.ceil(Math.random()*90) + 10);
+    this.score = 0;
     this.state = {
-      inputs: Array.apply(null, Array(10)).map(() => Array.apply(null, Array(10)).map(() => 0))
+      inputs: Array.apply(null, Array(10)).map(() => Array.apply(null, Array(10)).map(() => 0)),
+      evalute: false
     };
   }
 
@@ -64,7 +71,7 @@ export default class Body extends Component {
           {Array.apply(null, Array(10)).map((ele, colindex) => {
             return (
               <TableCell key={colindex}>
-                {this.state.inputs[rowindex][colindex] === this.rows[rowindex] + this.cols[colindex] &&
+                {this.state.inputs[rowindex][colindex] === this.rows[rowindex] + this.cols[colindex] && ++this.score &&
                   <Typography 
                     variant="body1"
                     color="primary"
@@ -88,22 +95,48 @@ export default class Body extends Component {
 
   render() {
     return (
-      <Table >
-        <TableHead>
-          <TableRow>
-            <TableCell key={0}></TableCell>
-            {this.cols.map((col, index) => {
-              return (
-                <TableCell key={index}>{col}</TableCell>
-              );
-            })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {!this.props.evalute && this.takeInput()}
-          {this.props.evalute && this.evaluate()}
-        </TableBody>
-      </Table>
+      <Card style={{margin: "50px", flexGrow: "1"}}>
+        <CardContent>
+          <Table >
+            <TableHead>
+              <TableRow>
+                <TableCell key={0}></TableCell>
+                {this.cols.map((col, index) => {
+                  return (
+                    <TableCell key={index}>{col}</TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!this.state.evalute && this.takeInput()}
+              {this.state.evalute && this.evaluate()}
+            </TableBody>
+          </Table>
+            <CardActions>
+              <Timer stop={this.state.evalute} style={{paddingLeft: "24px"}}/>
+              {this.state.evalute &&
+                <div style={{marginLeft: "auto"}}>
+                  <Typography 
+                    variant="title"
+                    color="inherit"
+                  >
+                  Score: {this.score} / 100
+                  </Typography>
+                </div>
+              }
+              <Button 
+                onClick={() => window.location.reload()}
+                style={{marginLeft: "auto"}}
+                color="primary"
+                variant="outlined"
+                size="small">
+                Reload
+              </Button>
+              <Button onClick={() => this.setState({evalute: true})} color="primary" variant="contained" size="small">Submit</Button>
+            </CardActions>
+          </CardContent>
+        </Card>
     );
   }
 }
